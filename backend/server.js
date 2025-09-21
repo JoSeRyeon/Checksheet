331c3f449381479cd18ca00f909662b19c502b36
JO SERYEON <tpfus011@naver.com>
2025-09-21 15:50:17 +0900

    use process.env.PORT and add configurable CORS origin

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const tasks = require('./routes/tasks.js');
-const checksheet =  require('./routes/checksheet.js');
-const code =  require('./routes/code.js');
-const task = require('./routes/task.js')
+const checksheet = require('./routes/checksheet.js');
+const code = require('./routes/code.js');
+const task = require('./routes/task.js');
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*',
+}));
 app.use(express.json());
 
 // router list
@@ -19,5 +20,5 @@ app.use('/api/code', code);
 app.use('/api/tasks', tasks);
 app.use('/api/task', task);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
